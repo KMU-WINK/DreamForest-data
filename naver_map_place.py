5d00f49fc2dc43474054cecf020bb0b44dfb6df7
JeongHeon Woo <email@wookingwoo.com>
2023-01-03 13:04:33 +0900

    :sparkles: Add get_store_info

--- naver_map_place.py
+++ naver_map_place.py
@@ -1,3 +1,4 @@
+import requests
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import json
@@ -22,8 +23,19 @@ def get_first_place_id(place_json_data):
     return first_place_id
 
 
+def get_store_info(place_id):
+    response = requests.get(f"https://map.naver.com/v5/api/sites/summary/{place_id}")
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        data = response.json()
+        return data
+
+
 if __name__ == '__main__':
     search_keyword = '짜짜루 서울 강남구 역삼동'
     place_json_data = get_search_list(search_keyword)
     place_id = get_first_place_id(place_json_data)
     print(place_id)
+    place_info = get_store_info(place_id)
+    print(place_info)
